a0bc2ff4fd81d69d0034ad4ed6ac825a4a4a6833
Raman Fedaseyeu <raman.fedaseyeu@gmail.com>
2021-07-03 23:09:49 +0200

    Rename `overloaded` to `overload`

--- escale_fw/src/overload.hpp
+++ escale_fw/src/overload.hpp
@@ -10,24 +10,24 @@
 //   https://stackoverflow.com/a/32476942/3541063
 
 template <typename... Ts>
-struct overloaded : Ts...
+struct overload : Ts...
 {
   // To support aggregate initialization (only) this template requires either
   // a user-defined deduction guide (until C++20)
   //
   //     template <typename... Ts>
-  //     overloaded(Ts...) -> overloaded<Ts...>;
+  //     overload(Ts...) -> overload<Ts...>;
   //
   // or a deleted constructor declaration
   //
-  //     overloaded(Ts...) = delete;
+  //     overload(Ts...) = delete;
   //
   // To support both the normal and aggregate initialization a proper
   // constructor declaration is required
   //
-  //     overloaded(Ts... ts) : Ts(ts)... {}
+  //     overload(Ts... ts) : Ts(ts)... {}
   //
-  overloaded(Ts...) = delete;
+  overload(Ts...) = delete;
 
   // g++ requires this explicit using-declaration, clang doesn't
   using Ts::operator()...;
