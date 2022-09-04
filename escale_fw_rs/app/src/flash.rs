de422ac1794577df5df2207e1464536c8d1fddf0
Raman Fedaseyeu <raman.fedaseyeu@gmail.com>
2022-09-04 20:35:02 +0200

    Minor improvements to "flash.rs"

--- escale_fw_rs/app/src/flash.rs
+++ escale_fw_rs/app/src/flash.rs
@@ -5,10 +5,20 @@ use core::{
 
 use rp2040_flash::flash;
 
+#[allow(non_upper_case_globals)]
+const MiB: usize = 1024 * 1024;
+
+/// XIP base address (see `XIP_BASE` in RP2040 datasheet).
 pub const FLASH_ORIGIN: usize = 0x10000000;
+/// RP2040 supports maximum 16 MiB of QSPI flash memory.
+pub const FLASH_END_MAX: usize = FLASH_ORIGIN + 16 * MiB;
+/// The erasable sector size.
 const FLASH_SECTOR_SIZE: usize = 4096;
+/// The value an erased sector is filled with. This is typically 0xff.
 const FLASH_ERASED_VALUE: u8 = 0xff;
 
+/// The payload type should be `repr(C)` to have a stable layout,
+/// because the flash-stored payload can survive firmware upgrades.
 pub union Flash<T>
 where
     T: Copy,
@@ -41,16 +51,24 @@ where
         instance
     }
 
-    pub fn read(mem_addr: usize) -> Self {
+    pub unsafe fn read(mem_addr: usize) -> Self {
+        assert!(mem_addr >= FLASH_ORIGIN);
+        assert!(mem_addr <= FLASH_END_MAX - size_of::<Self>());
+        // The read address must be sector-aligned, because the write function
+        // only ever allows writing at sector-aligned addresses.
+        assert!(is_aligned(mem_addr, FLASH_SECTOR_SIZE));
+
         let mut flash_sector = Flash::default();
         flash_sector.value = unsafe { read_volatile(mem_addr as *const _) };
         flash_sector
     }
 
     pub unsafe fn write(&self, mem_addr: usize) {
-        let flash_addr = mem_addr - FLASH_ORIGIN;
-        assert!(is_aligned(flash_addr, FLASH_SECTOR_SIZE));
+        assert!(mem_addr >= FLASH_ORIGIN);
+        assert!(mem_addr <= FLASH_END_MAX - size_of::<Self>());
+        assert!(is_aligned(mem_addr, FLASH_SECTOR_SIZE));
 
+        let flash_addr = mem_addr - FLASH_ORIGIN;
         flash::flash_range_erase_and_program(flash_addr as u32, &self.data, true);
     }
 
@@ -60,7 +78,7 @@ where
 }
 
 pub const fn sector_aligned_size_of<T>() -> usize {
-    assert!(is_pwr_of_two(FLASH_SECTOR_SIZE));
+    assert!(FLASH_SECTOR_SIZE.is_power_of_two());
     ceil_step(size_of::<T>(), FLASH_SECTOR_SIZE)
 }
 
@@ -75,10 +93,6 @@ const fn ceil_step(value: usize, step: usize) -> usize {
 }
 
 const fn is_aligned(addr: usize, alignment: usize) -> bool {
-    assert!(is_pwr_of_two(alignment));
+    assert!(alignment.is_power_of_two());
     addr & (alignment - 1) == 0
-}
-
-const fn is_pwr_of_two(n: usize) -> bool {
-    n != 0 && (n & (n - 1) == 0)
 }
