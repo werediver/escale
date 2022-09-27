bb6dc59e0e95f0d2d712c7bf850ee26f7528bf26
Raman Fedaseyeu <raman.fedaseyeu@gmail.com>
2022-09-27 20:14:08 +0200

    Introduce `Ring.retain()`, `.retain_mut()`, `.append()`

--- escale_fw_rs/lib/ring/src/ring.rs
+++ escale_fw_rs/lib/ring/src/ring.rs
@@ -1,6 +1,7 @@
 use core::{
     mem::MaybeUninit,
     ops::{Index, IndexMut},
+    ptr::drop_in_place,
 };
 
 use crate::ring_state::{Behavior, Error, Overwriting, Push, RingState, Saturating};
@@ -73,13 +74,38 @@ impl<T, const N: usize, B: Behavior> Ring<T, N, B> {
         Ok(unsafe { self.take(i) })
     }
 
-    // pub fn retain<F>(&mut self, f: F)
-    // where
-    //     F: FnMut(&T),
-    // {
-    //     for
-    // }
+    pub fn retain_mut<F>(&mut self, mut f: F)
+    where
+        F: FnMut(&mut T) -> bool,
+    {
+        let mut i: usize = 0;
+        let mut removed_count = 0;
+        let count = self.state.count();
+        while i < count - removed_count {
+            let item = unsafe { self.peek_mut(self.state.index(i + removed_count).unwrap()) };
+            let should_retain = f(item);
+            if should_retain {
+                if removed_count > 0 {
+                    self.data[i] = MaybeUninit::new(unsafe { self.take(i + removed_count) });
+                }
+                i += 1;
+            } else {
+                unsafe { drop_in_place(item) };
+                removed_count += 1;
+            }
+        }
+        // To rectify this `RingState` should support `retain()` "natively".
+        for _ in 0..removed_count {
+            _ = self.state.will_pop_back();
+        }
+    }
 
+    pub fn retain<F>(&mut self, mut f: F)
+    where
+        F: FnMut(&T) -> bool,
+    {
+        self.retain_mut(|value| f(value))
+    }
     unsafe fn take(&mut self, index: usize) -> T {
         self.data[index].assume_init_read()
     }
@@ -105,6 +131,16 @@ impl<T, const N: usize> Ring<T, N, Saturating> {
         self.data[i] = MaybeUninit::new(value);
         Ok(())
     }
+
+    pub fn append<TT>(&mut self, items: TT) -> Result<(), Error>
+    where
+        TT: IntoIterator<Item = T>,
+    {
+        for item in items {
+            self.push(item)?;
+        }
+        Ok(())
+    }
 }
 
 impl<T, const N: usize> Ring<T, N, Overwriting> {
@@ -126,6 +162,15 @@ impl<T, const N: usize> Ring<T, N, Overwriting> {
         };
         self.data[i] = MaybeUninit::new(value);
         Ok(displaced)
+    }
+
+    pub fn append<TT>(&mut self, items: TT)
+    where
+        TT: IntoIterator<Item = T>,
+    {
+        for item in items {
+            _ = self.push(item);
+        }
     }
 }
 
@@ -247,5 +292,49 @@ mod tests {
         r[1] = 20;
         assert_eq!(r[0], 10);
         assert_eq!(r[1], 20);
+    }
+
+    #[test]
+    fn sat_retain_mut_drop_all() {
+        let mut r: Ring<i32, 2, Saturating> = Default::default();
+        assert_eq!(r.push_front(1), Ok(()));
+        assert_eq!(r.push_front(2), Ok(()));
+        r.retain_mut(|_| false);
+        assert!(r.is_empty());
+    }
+
+    #[test]
+    fn owr_retain_mut_drop_all() {
+        let mut r: Ring<i32, 2, Overwriting> = Default::default();
+        assert_eq!(r.push_front(1), Ok(None));
+        assert_eq!(r.push_front(2), Ok(None));
+        r.retain_mut(|_| false);
+        assert!(r.is_empty());
+    }
+
+    #[test]
+    fn sat_retain_mut_modify() {
+        let mut r: Ring<i32, 2, Saturating> = Default::default();
+        assert_eq!(r.push_front(1), Ok(()));
+        assert_eq!(r.push_front(2), Ok(()));
+        r.retain_mut(|value| {
+            *value += 1;
+            true
+        });
+        assert_eq!(r.pop(), Ok(3));
+        assert_eq!(r.pop(), Ok(2));
+    }
+
+    #[test]
+    fn owr_retain_mut_modify() {
+        let mut r: Ring<i32, 2, Overwriting> = Default::default();
+        assert_eq!(r.push_front(1), Ok(None));
+        assert_eq!(r.push_front(2), Ok(None));
+        r.retain_mut(|value| {
+            *value += 1;
+            true
+        });
+        assert_eq!(r.pop(), Ok(3));
+        assert_eq!(r.pop(), Ok(2));
     }
 }
